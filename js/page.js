cef1f836bd58a2d0e58c7c8334e2308adfae0351
Ado13e <77327642+properza@users.noreply.github.com>
2024-01-09 22:26:58 +0700

    Fixed

---
+++ js/page.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const scheduleHeader = document.getElementById("news");
+  const resultsHeader = document.getElementById("contact");
+  const scheduleLink = document.querySelector(".menu a[href='#news']");
+  const resultsLink = document.querySelector(".menu a[href='#contact']");
+
+  let isScheduleClicked = false;
+  let isResultsClicked = false;
+
+  scheduleLink.addEventListener("click", function (event) {
+    // event.preventDefault();
+    if (!isScheduleClicked) {
+      scheduleHeader.style.marginTop = "10rem";
+      isScheduleClicked = true;
+      isResultsClicked = false;
+    } else {
+      scheduleHeader.style.marginTop = "0";
+      isScheduleClicked = false;
+    }
+  });
+
+  resultsLink.addEventListener("click", function (event) {
+    // event.preventDefault();
+    if (!isResultsClicked) {
+      resultsHeader.style.marginTop = "10rem";
+      isResultsClicked = true;
+      isScheduleClicked = false;
+    } else {
+      resultsHeader.style.marginTop = "0";
+      isResultsClicked = false;
+    }
+  });
+});
